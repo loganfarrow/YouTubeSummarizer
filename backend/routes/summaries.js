be51af3508bad32e815b0a61085b57dadadcc687
Ben Mueller <benjm2002@gmail.com>
2023-07-06 23:51:09 -0500

    created express backend, outlined endpoints related to summaries

---
+++ backend/routes/summaries.js
@@ -0,0 +1,51 @@
+require('dotenv').config()
+const express = require('express')
+// const { check, validationResult } = require('express-validator');
+
+const router = express.Router()
+
+// fetch all summaries from user (in chronological order with option to reverse order)
+router.get('/:userId(\\d+)', (req, res) => {
+    if ('reverse' in req.query) {
+        if (req.query.reverse === 'true') {
+            // TODO fetch summaries in reverse order
+        }
+    }
+    else {
+        // TODO fetch summaries in chronological order        
+    }
+
+    res.status(200).json({ message: 'hello world' })
+})
+
+// fetch specific summary from user
+router.get('/:userId(\\d+)/fetchSummary/:summaryId(\\d+)', (req, res) => {
+    res.status(200).json({ message: 'hello world' })
+})
+
+// fetch only summaries with given search text in title
+router.get('/:userId(\\d+)/findSummary', (req, res) => {
+    let searchText = req.query('searchText').toString()
+    if (!searchText.isString()) {
+        res.status(400).json({ error: 'The summary search query must be a string' })
+    }
+    res.status(200).json({ message: 'hello world' })
+})
+
+// update specific summary title
+router.patch('/:userId(\\d+)/getSummaryTitle/:summaryId(\\d+)', (req, res) => {
+    res.status(200).json({ message: 'hello world' })
+})
+
+// delete specific summary
+router.delete('/:userId(\\d+)/deleteSummary/:summaryId(\\d+)', (req, res) => {
+    res.status(200).json({ message: 'hello world' })
+})
+
+// create new summary
+router.post('/:userId(\\d+)/createSummary', (req, res) => {
+    res.status(200).json({ message: 'hello world' })
+})
+
+
+module.exports = router
---
+++ backend/server.js
@@ -0,0 +1,69 @@
+// includes environment variables
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+
+const authenticationRoutes = require('./routes/authentication')
+const summaryRoutes = require('./routes/summaries')
+
+const app = express()
+
+// define middleware here (middleware is called on requests in the order it's defined)
+app.use(express.json())
+
+// debugging middleware to print out any request and method that comes in
+app.use((req, res, next) => {
+    console.log(req.method, ' request to ', req.path, ' with body ', req.body)
+    next()
+})
+
+// point to the route files (which contain the endpoints)
+app.use('/api/authentication', authenticationRoutes)
+app.use('/api/summaries', summaryRoutes)
+
+// if no routes match, return 404 and remind them to only use integer paramters for userId and summaryId
+app.use((req, res) => {
+    res.status(404).json({ error: 'Route not found, make sure you are passing in integer paramters for userId and summaryId in the url' });
+});
+
+// check for and log errors
+app.use((err, req, res, next) => {
+    console.error('ERROR: ', err); // Log the error for debugging purposes
+  
+    res.status(500).json({ error: 'Internal Server Error' });
+});
+
+// connect to db
+mongoose.connect(process.env.MONG_URI)
+    .then(() => {
+        console.log('connected to mongodb atlas')
+    })
+    .catch((error) => {
+        console.log('error connecting to mongodb atlas: ', error)
+    })
+
+// listen for request to backend
+app.listen(process.env.PORT, () => {
+    console.log(`api is running on port ${process.env.PORT}`)
+})
+
+
+// endpoints to make
+// 1. fetch all summaries for given user      GET     /api/summaries/:userId
+    // should return in chronological order (maybe include query argument to display in reverse order)
+// 2. fetch specific summary from user's summaries  GET /api/summaries/:userId/:summaryId
+// 2 1/2. fetch only summaries with given search text in title  GET /api/summaries/:userId/findSummary?searchText=...
+// 3. update specific summary title  PATCH /api/summaries/:userId/:summaryId
+// 4. delete specific summary  DELETE /api/summaries/:userId/:summaryId
+// 5. create new summary  POST /api/summaries/:userId
+//
+// TODO these will probably need to be modified
+// 6. create new user POST /api/users
+// 7. delete user DELETE /api/users/:userId
+// 8. update user data PATCH /api/users/:userId
+// 9. fetch user data GET /api/users/:userId  (is this needed?)
+//
+// TODO figure these out
+// 10. log in user POST /api/login    [create the session]
+// 11. log out user DELETE /api/logout    [end the session]
