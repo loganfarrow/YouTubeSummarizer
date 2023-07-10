66eff3858155c24808518882896934d284dcbe15
Ben Mueller <benjm2002@gmail.com>
2023-07-09 22:14:31 -0500

    outlined authentication work, need to implement now

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,5 +5,29 @@ const mongoose = require('mongoose')
         
 */
 const userSchema = new mongoose.Schema({
-    username: String,
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 'Email already exists',
+        match: [/.+\@.+\..+/, 'Please fill a valid email address'],
+        required: true,
+    },
+    hashed_password: {
+        type: String,
+        required: true,
+    },
+    hashed_openai_key: {
+        type: String,
+        required: false,
+    },
+    dateCreated: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    }
 })
