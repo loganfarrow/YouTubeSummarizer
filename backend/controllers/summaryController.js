b47e277a0ab897ccf55f39f46e763fe517a8b6c7
Ben Mueller <benjm2002@gmail.com>
2023-07-09 21:21:04 -0500

    finished 2 endpoints

--- backend/controllers/summaryController.js
+++ backend/controllers/summaryController.js
@@ -1,17 +1,27 @@
 const Summary = require('../models/Summary')
+const User = require('../models/User')
 
-exports.fetchAllFromUser = (req, res) => {
+exports.fetchAllFromUser = async (req, res) => {
+    // fetch all from user in chronological order
     let summaries = []
-    summaries = Summary.find({ user: req.params.userId }).sort({ dateCreated: -1 })
+    try {
+        summaries = await Summary.find({ user: req.params.userId }).sort({ dateCreated: 1 })
 
-    if ('reverse' in req.query) {
-        if (req.query.reverse === 'true') {
-            summaries.sort({ dateCreated: -1 })
+        // reverse date order if requested
+        if ('reverse' in req.query) {
+            if (req.query.reverse === 'true') {
+                summaries.sort({ dateCreated: -1 })
+            }
         }
     }
-    
+    catch (e) {
+        console.error(e.message)
+        res.status(400).json({ error: 'The following error occurred while getting users summaries: ' + e.message })
+    }
 
-    res.status(200).json({ message: 'hello world' })
+    console.log(summaries)
+
+    res.status(200).json(summaries)
 }
 
 exports.fetchOneFromUser = (req, res) => {
@@ -34,8 +44,44 @@ exports.deleteSummary = (req, res) => {
     res.status(200).json({ message: 'hello world' })
 }
 
-exports.createSummary = (req, res) => {
-    // const {name, summary, etc} = req.body
+exports.createSummary = async (req, res) => {
+    const { title, summary, options } = req.body
+    const userId = req.params.userId
+    const dateCreated = new Date()
 
-    res.status(200).json({ message: 'hello world' })
+    let user = null
+    try {
+        user = await User.findById(userId)
+    }
+    catch (e) {
+        console.error(e.message)
+        res.status(400).json({ error: 'The following error occurred while getting user: ' + e.message })
+    }
+    
+    let optionsJSON = ''
+    try {
+        optionsJSON = JSON.parse(options)
+    } catch {
+        console.error('UNable to parse summary options dictionary to JSON')
+    }
+
+    let savedSummary = null
+    try {
+        const newSummary = new Summary({
+            title: title,
+            summary: summary,
+            options: optionsJSON,
+            dateCreated: dateCreated,
+            user: user
+        })
+        savedSummary = await newSummary.save()
+    } catch (e) {
+        console.error(e.message)
+        res.status(400).json({ error: 'The following error occurred while saving summary: ' + e.message })
+    }
+
+    res.status(200).json({
+        message: 'Summary successfully created',
+        summary: savedSummary
+    })
 }
