9fcd9c87c90583663ea7492e013825806e22525a
Ben Mueller <benjm2002@gmail.com>
2023-07-19 22:09:52 -0500

    fixing authentication middleware bugs and debugging a few endpoints

--- backend/utils/error_messages.js
+++ backend/utils/error_messages.js
@@ -1,6 +1,6 @@
 const provideEmailAndPasswordToRegister = 'Must provide email and password when registering'
 const provideValidEmailToRegister = 'Must provide valid email when registering'
-const provideStrongPasswordToRegister = 'Must provide strong password when registering'
+const provideStrongPasswordToRegister = 'Must provide strong password when registering (min 8 characters, 1 uppercase, 1 lowercase, 1 number, 1 symbol)'
 const userAlreadyExists = 'Cannot signup with email that is already associated with an account'
 const userDoesNotExistForId = 'No user is associated with the provided id'
 const failedToDeleteUser = 'Failed to delete user'
@@ -11,7 +11,7 @@ const incorrectPassword = 'Incorrect password'
 // for when there is no auth header
 const authorizationRequired = 'You must be logged in to access this endpoint (authorization header not found, token is needed)'
 // for when there is an auth header but the token is invalid
-const unauthorizedToken = 'You must be logged in to access this endpoint (token in authorization header is invalid)'
+const unauthorizedToken = 'You must be logged in to access this endpoint (token in authorization header is invalid). If you believe token is valid, it is possible the associated user was deleted.'
 
 // for when no openai key is provided when needed
 const noOpenaiKeyProvided = 'No openai key provided for endpoint that requires a valid openai key'
