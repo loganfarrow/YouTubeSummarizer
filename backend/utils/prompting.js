cb0760acd7b6267e651a2a102304b8a6e1ea5f2f
benmueller-wk <ben.mueller@workiva.com>
2023-08-17 01:15:04 -0500

    fixed export syntax error

--- backend/utils/prompting.js
+++ backend/utils/prompting.js
@@ -1,4 +1,4 @@
-export const customInstructions = `
+const customInstructions = `
     TODO these will be the custom instructions that are universal to all prompts
 `
 
@@ -6,10 +6,15 @@ const defaultPrompt = `
     TODO this will be the part of the prompt that is the same no matter what is in options dictionary
 `
 
-export function getPrompt(options) {
+function getPrompt(options) {
     let prompt = defaultPrompt
 
     // TODO add logic here to add to the prompt based on the different values in options dict (if they exist)
 
     return prompt
+}
+
+module.exports = {
+    getPrompt,
+    customInstructions,
 }
