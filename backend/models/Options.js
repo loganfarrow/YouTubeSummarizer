1926e2b537249bc0fdcf3d5018c9f4fd94673c43
Ben Mueller <benjm2002@gmail.com>
2023-08-18 23:14:12 -0500

    attempting to create options object with invalid field no longer crashed application

--- backend/models/Options.js
+++ backend/models/Options.js
@@ -38,14 +38,46 @@ const optionsSchema = new mongoose.Schema({
 
 // take in a json object and creates an Options instance out of it
 optionsSchema.statics.createNewOptions = async function (options) {
+    const acceptableOptions = ['length', 'tone', 'targetAge', 'bulletPoints', 'paragraphLimit', 'wordLimit', 'bulletPointLimit']
+    
+    // ensure that all the options provided are valid
+    for (const key in options) {
+        if (!acceptableOptions.includes(key)) {
+            throw new Error('invalid option provided in options dictionary: ' + key)
+        }
+    }
 
     const { length, tone, targetAge, bulletPoints, paragraphLimit, wordLimit, bulletPointLimit } = options
+
+    // ensure field values are valid
+    if ((length && !['tiny', 'short', 'medium', 'long', 'xlong'].includes(length))) {
+        throw new Error('invalid length value provided in options dictionary: ' + length)
+    }
+    if (tone && !['standard', 'professional', 'academic', 'casual', 'einstein', 'redneck', 'dog'].includes(tone)) {
+        throw new Error('invalid tone value provided in options dictionary: ' + tone)
+    }
+    if (targetAge && !['unspecified', 'five year old', 'teenager', 'college student', 'adult'].includes(targetAge)) {
+        throw new Error('invalid targetAge value provided in options dictionary: ' + targetAge)
+    }
+    if (bulletPoints && typeof bulletPoints !== 'boolean') {
+        throw new Error('bulletPoints value must be a boolean')
+    }
+    if ((paragraphLimit && typeof paragraphLimit !== 'number') || typeof paragraphLimit === 'boolean') {
+        throw new Error('paragraphLimit value must be a number')
+    }
+    if ((wordLimit && typeof wordLimit !== 'number') || typeof wordLimit === 'boolean') {
+        throw new Error('wordLimit value must be a number')
+    }
+    if ((bulletPointLimit && typeof bulletPointLimit !== 'number') || typeof bulletPointLimit === 'boolean') {
+        throw new Error('bulletPointLimit value must be a number')
+    }
+
     return new this({
         length: length || 'medium',
         tone: tone || 'standard',
         targetAge: targetAge || 'unspecified',
         bulletPoints: bulletPoints || false,
-        wordLimit: wordLimit || 500,
+        wordLimit: wordLimit || 300,
         paragraphLimit: paragraphLimit,
         bulletPointLimit: bulletPointLimit,
     })
