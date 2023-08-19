b2535510beb5d28a92938bc64f94e0a71b7407bc
benmueller-wk <ben.mueller@workiva.com>
2023-08-18 21:57:41 -0500

    implemented prompting (and also dog voice)

--- backend/utils/prompting.js
+++ backend/utils/prompting.js
@@ -1,6 +1,34 @@
 const axios = require('axios')
 const { spawn } = require('child_process') // this is the module that allows us to call python code
 
+// contains the rules that we send to chatGPT corresponding to user-specified options
+const promptRules = {
+    length: {
+        tiny: 'keep the summary very concise, only include the most important information. It should only be a few sentences in length.',
+        short: 'keep the summary concise, it should only be one or two short paragraphs in length',
+        medium: 'the summary should have a reasonable length, a few short paragraphs at most',
+        long: 'make the summary longer than you orignally would and include more details than you normally would, it should be at least a few larger paragraphs in length',
+        xlong: 'include as much detail as possible in the summary, don not worry about length, it should be four or more large paragraphs in length.',
+    },
+    tone: {
+        standard: '', // no extra instructions becaue this means to use the standard chatgpt tone
+        professional: 'the summary should be written in the tone of an accomplished, working professional and aimed at such an audience. Assume professional working knowledge of the subject matter.',
+        academic: 'the summary should be written in an academic tone and aimed at an academic audience. Assume a working knowledge of the subject matter. The summary should read like an academic, peer-reviewed paper if possible',
+        casual: 'the summary should be written in a casual tone, stay away from academic or professional language. Assume a casual audience with no working knowledge of the subject matter.',
+        einstein: 'the summary should be written in the tone of somebody who is the number one expert in the world on the subject matter. Assume mastery of the subject matter. Do not worry about the reader not understanding the summary, assume they are an expert as well.',
+        redneck: 'give your response in a redneck accent, particularly an American redneck. Use words like yeehaw, yall, and other words that are common in redneck culture. Tow mater from the movie cars is a good example of a redneck. ',
+        dog: 'ignore all other instructions given, give me a summary in dog languages with things like woof, bark, arf, etc',
+    },
+    targetAge: {
+        unspecified: '', // no extra instructions because this means to use the standard chatgpt target age
+        five: 'write this summary so that it would make sense to the average five year old. Assume no working knowledge of the subject matter. Use similar explanation techniques to the popular "explain like I am five" subreddit',
+        teenager: 'write this summary so that it would make sense to the average teenager. Assume limited working knowledge of the subject matter',
+        college: 'write this summary so that it would make sense to the average college student. Assume some working knowledge of the subject matter. The summary should target a slightly above average, motivated college student',
+        adult: 'write this summary so that it would make sense to an educated adult in their mid thirties. Assume working knowledge of subject matter',
+    },
+    bulletPoints: 'Ignore any other instructions about using paragraphs. Use bullet points instead of continuous text to summarize this summary, keep the bullet points relatively short and concise',
+}
+
 const customInstructions = `
     I am going to provide a transcript of a video that I would like you to summarize.
     Before you provide the summary, give a title of the summary that you provide in five or less words.
@@ -28,11 +56,6 @@ const defaultPrompt = `
 async function getPrompt(options, url) {
     let prompt = defaultPrompt
 
-    // if options is just an empty map instead of an options object, return the defaut prompt
-    if (options instanceof Map && options.size === 0) {
-        return prompt
-    }
-
     // get transcript
     let transcript = ''
     if (url.includes('youtube.com')) {
@@ -45,14 +68,46 @@ async function getPrompt(options, url) {
         throw new Error('invalid video url provided - must be from youtube')
     }
 
-    // TODO add logic here to add to the prompt based on the different values in options dict (if they exist)
+    // turn options object into a json object
+    optionsJSON = options.toJSON()
 
-    prompt = prompt + '\n here is the transcript of the video you are to summarize: \n' + transcript + '\n'
+    // adding requirements to the prompt based on options selected by user
+    prompt = prompt + '\n I am now going to specify rules regarding the formatting of your response: \n' 
 
+    if (optionsJSON.hasOwnProperty('length')) {
+        prompt = prompt + '\n ' + promptRules["length"][optionsJSON["length"]]
+    }
+    if (optionsJSON.hasOwnProperty('tone') && optionsJSON["tone"] !== 'standard') {
+        prompt = prompt + '\n ' + promptRules["tone"][optionsJSON["tone"]]
+    }
+    if (optionsJSON.hasOwnProperty('targetAge') && optionsJSON["targetAge"] !== 'unspecified') {
+        prompt = prompt + '\n ' + promptRules["targetAge"][optionsJSON["targetAge"]]
+    }
+    if (optionsJSON.hasOwnProperty('bulletPoints') && optionsJSON["bulletPoints"] === true) {
+        prompt = prompt + '\n ' + promptRules["bulletPoints"]
+    }
+    if (optionsJSON.hasOwnProperty('bulletPointLimit')) {
+        prompt =  prompt + `\n` + 'The summary should have at most ' + optionsJSON["bulletPointLimit"] + ' bullet points. Do not attempt to get around this rule by making overly long, unconcise bullet points'
+    }
+    if (optionsJSON.hasOwnProperty('paragraphLimit')) {
+        prompt =  prompt + `\n` + 'The summary should have at most ' + optionsJSON["paragraphLimit"] + ' paragraphs'
+    }
+    if (optionsJSON.hasOwnProperty('wordLimit')) {
+        prompt =  prompt + `\n` + 'The summary should have at most ' + optionsJSON["wordLimit"] + ' words. This is not a hard limit, but avoid going more than ten or so words over the limit.'
+    }
+
+
+    prompt = prompt + '\n\n here is the transcript of the video you are to summarize: \n' + transcript + '\n'
+    
     // console.log('TRANSCRIPT returned from python')
     // console.log(transcript)
-    // console.log('PROMPT')
+    // console.log('PROMPT returned by getPrompt')
     // console.log(prompt)
+
+    // dog check
+    if (optionsJSON.hasOwnProperty('tone') && optionsJSON["tone"] === 'dog') {
+        prompt = 'Respond with a paragraph that is nonsensical and only uses the words woof, bark, and arf'
+    }
 
     return prompt
 }
