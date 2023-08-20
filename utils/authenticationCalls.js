803c4c9f3e874bfffba309d435566c6fe21284be
Ben Muellerexport <benjm2002@gmail.com>
2023-08-20 17:27:50 -0500

    authenticanCalls backend calls now handle errros

--- utils/authenticationCalls.js
+++ utils/authenticationCalls.js
@@ -1,64 +1,81 @@
 const axios = require('axios')
-const { get_auth_header } = require('./misc.js')
+const { get_auth_header, generic_error_handler } = require('./misc.js')
 
 const BACKEND_URL_BASE = "http://localhost:4000/api/authentication"
 
 // returns { email, jwt_token}
 export const login = async (email, password) => {
-    const response = await axios.post(`${BACKEND_URL_BASE}/login`, {
+    return axios.post(`${BACKEND_URL_BASE}/login`, {
         email,
         password
     })
-    return { data: response.data, status: response.status }
+    .then((response) => {
+        return { data: response.data, status: response.status }
+    })
+    .catch((e) => generic_error_handler(e))
 }
 
 // returns { email, jwt_token}
 export const register = async (email, password, openaikey) => {
-    const response = await axios.post(`${BACKEND_URL_BASE}/register`, {
+    return axios.post(`${BACKEND_URL_BASE}/register`, {
         email,
         password,
         openaikey
     })
-    return { data: response.data, status: response.status }
+    .then((response) => {
+        return { data: response.data, status: response.status }
+    })
+    .catch((e) => generic_error_handler(e))
 }
 
 // returns a success or error message
 export const updateOpenAiKey = async (openaiKey, jwt) => {
-    const response = await axios.patch(`${BACKEND_URL_BASE}/updateOpenAiKey`, {
+    return axios.patch(`${BACKEND_URL_BASE}/updateOpenAiKey`, {
         openaiKey: openaiKey
     }, { headers: get_auth_header(jwt) })
-
-    return { data: response.data, status: response.status }
+    .then((response) => {
+        return { data: response.data, status: response.status }
+    })
+    .catch((e) => generic_error_handler(e))
 }
 
 // returns info on the user currently logged in {email, createdAt}
-export const fetchUser = async (jwt) => {
-    auth_header = {'Authorization': `${jwt}`}
-    const response = await axios.get(`${BACKEND_URL_BASE}/fetchUser`, { headers: get_auth_header(jwt) })
-    return { data: response.data, status: response.status }
+export const fetchUser = async (jwt) => {    
+    return axios.get(`${BACKEND_URL_BASE}/fetchUser`, { headers: get_auth_header(jwt) })
+    .then((response) => {
+        return { data: response.data, status: response.status }
+    })
+    .catch((e) => generic_error_handler(e))
 }
 
 // returns success or error messaage
 export const updatePassword = async (newPassword, jwt) => {
-    auth_header = {'Authorization': `${jwt}`}
-    const response = await axios.patch(`${BACKEND_URL_BASE}/updatePassword`, {
+    return axios.patch(`${BACKEND_URL_BASE}/updatePassword`, {
         newPassword: newPassword
     }, { headers: get_auth_header(jwt) })
-    return { data: response.data, status: response.status }
+    .then((response) => {
+        return { data: response.data, status: response.status }
+    })
+    .catch((e) => generic_error_handler(e))
 }
 
 // returns success or error message
 export const updateEmail = async (newEmail, jwt) => {
-    auth_header = {'Authorization': `${jwt}`}
-    const response = await axios.patch(`${BACKEND_URL_BASE}/updateEmail`, {
+    return axios.patch(`${BACKEND_URL_BASE}/updateEmail`, {
         newEmail: newEmail
     }, { headers: get_auth_header(jwt) })
-    return { data: response.data, status: response.status }
+    .then((response) => {
+        return { data: response.data, status: response.status }
+    })
+    .catch((e) => generic_error_handler(e))
 }
 
 // returns success or error message
 export const deleteUser = async (jwt) => {
-    auth_header = {'Authorization': `${jwt}`}
-    const response = await axios.delete(`${BACKEND_URL_BASE}/deleteUser`, { headers: get_auth_header(jwt) })
-    return { data: response.data, status: response.status }
+    return axios.delete(`${BACKEND_URL_BASE}/deleteUser`, { headers: get_auth_header(jwt) })
+    .then((response) => {
+        return { data: response.data, status: response.status }
+    })
+    .catch((e) => generic_error_handler(e))
 }
+
